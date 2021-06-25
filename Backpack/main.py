f7055c737175abf50508452910ffe516427a1ad6
leonardodemicheli <leo.demicheli@icloud.com>
2021-06-25 14:43:32 +0200

    Files

--- Backpack/main.py
+++ Backpack/main.py
@@ -1,2 +1,12 @@
-ba = ['r', 'er', 'ger', 'gern']
-print(ba)
+student = {'name' : 'Alex', 'age' : 13}
+
+student.update({'street' : 'Gartenstr. 1'})
+student.update({'name' : 'Alex2'})
+del student['age']
+print(student)
+
+students =[{'name' : 'Alex', 'age' : 13}, {'name' : 'Alex3', 'age' : 14}]
+print(students)
+
+
+print('hallo')
